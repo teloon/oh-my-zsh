98d7518ba2d6bb0a48ccff2d5b36d6d7be2458b5
Yan Wu <v@tankexchange.co>
2016-08-15 18:32:13 -0700

    add goinit function

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -120,3 +120,8 @@ export PIP_REQUIRE_VIRTUALENV=true
 export JAVA_HOME=$(/usr/libexec/java_home)
 export EC2_HOME=/usr/local/ec2/ec2-api-tools-1.7.5.1
 export PATH=$PATH:$EC2_HOME/bin 
+
+function goinit() {
+    mkdir -p src bin pkg
+    export GOPATH=$PWD
+}
